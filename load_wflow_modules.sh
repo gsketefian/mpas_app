36d4fb61b6100c4ea4f49cb0ba26accbe9d03c1a
gerard ketefian <gerard.ketefian@noaa.gov>
2025-02-25 11:38:37 -0700

    Add better error message/behavior to load_wflow_modules.sh.

--- load_wflow_modules.sh
+++ load_wflow_modules.sh
@@ -2,7 +2,12 @@
 scrfunc_fp=$( readlink -f "${BASH_SOURCE[0]}" )
 scrfunc_dir=$( dirname "${scrfunc_fp}" )
 
-module use $scrfunc_dir/modulefiles
-module load wflow_$1 > /dev/null 2>&1
+if [ -z "${1}" ]; then
+  echo "You must provide the platform name as first argument"
+  exit 1
+else
+  module use $scrfunc_dir/modulefiles
+  module load wflow_$1 > /dev/null 2>&1
 
-conda activate mpas_app
+  conda activate mpas_app
+fi
