ce8fd1f4a925eb88941b6c28a132998b0153bdb7
gerard ketefian <gerard.ketefian@noaa.gov>
2025-08-15 13:39:17 -0600

    Comment out line that loads the spack-stack version of intel since that was on disks /scratch[1,2] on hera, which are no longer available.

--- modulefiles/build_hera_intel.lua
+++ modulefiles/build_hera_intel.lua
@@ -6,7 +6,12 @@ the NOAA RDHPC machine Hera using Intel-2022.1.2
 whatis([===[Loads libraries needed for building the MPAS App on Hera ]===])
 prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/spack-stack/spack-stack-1.5.1/envs/unified-env-rocky8/install/modulefiles/Core")
 
-load("stack-intel/2021.5.0")
+-- Comment out loading of intel in spack-stack because the latter was on
+-- scratch1 or scratch2, which have now been replaced with scratch[3,4].
+-- Probably the above prepend_path() can be fixed to point to the correct
+-- new spack-stack path, but just commenting out the following line seems
+-- to be sufficient.
+-- load("stack-intel/2021.5.0")
 load("cmake/3.28.1")
 load("gnu")
 load("intel/2023.2.0")
